b0f40992257d06a4b47abc58e3e994cf94d098c2
ezirmusitua <jferroal@gmail.com>
2018-06-23 14:40:18 +0800

    Feat: add default jupyter notebook config

---
+++ jupyter/jupyter_notebook_config.py
@@ -0,0 +1,13 @@
+c.Application.log_datefmt = '%Y-%m-%d %H:%M:%S'
+c.Application.log_format = '[%(name)s]%(highlevel)s %(message)s'
+c.Application.log_level = 30
+c.JupyterApp.answer_yes = True
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.allow_password_change = False
+c.NotebookApp.allow_root = True  # Run in docker
+c.NotebookApp.certfile = u'path/to/ssl-cert-file'
+c.NotebookApp.keyfile = u'path/to/ssl-key-file'
+c.NotebookApp.password = u'password__hashed'
+c.NotebookApp.open_browser = False
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8888  # Update with whatever you want
