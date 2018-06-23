79b0e05c1e005cd715572451319de7ecd9e6013c
ezirmusitua <jferroal@gmail.com>
2018-06-23 17:54:50 +0800

    Feat: add nginx cerbot image

--- jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter_notebook_config.py
@@ -5,8 +5,9 @@ c.JupyterApp.answer_yes = True
 c.NotebookApp.allow_origin = '*'
 c.NotebookApp.allow_password_change = False
 c.NotebookApp.allow_root = True  # Run in docker
-c.NotebookApp.certfile = u'path/to/ssl-cert-file'
-c.NotebookApp.keyfile = u'path/to/ssl-key-file'
+# Uncomment if you did not use nginx for forwarding
+# c.NotebookApp.certfile = u'path/to/ssl-cert-file'
+# c.NotebookApp.keyfile = u'path/to/ssl-key-file'
 c.NotebookApp.password = u'password__hashed'
 c.NotebookApp.open_browser = False
 c.NotebookApp.ip = '*'
---
+++ nginx-cerbot/init.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# handling cert webroot nginx config and other conf
+sed -i 's/#domain-list#/$1/g' cert.conf
+mv cert.conf /etc/nginx/conf.d/
+sed -i '$d' /etc/nginx/nginx.conf
+sed -i '$a\ \ \ \ include '$4';' /etc/nginx/nginx.conf
+sed -i '$a}' /etc/nginx/nginx.conf
+
+# move cert-webroot to home
+mv cert-webroot ~
+
+# reload nginx
+nginx -s reload
+
+# exec cerbot
+certbot -d $1 --authenticator $2 --installer nginx -n -m $3 --agree-tos
+
+# add renew cert cron task
+crontab -u root autorenew.txt
+
+# reload nginx
+nginx -s reload
