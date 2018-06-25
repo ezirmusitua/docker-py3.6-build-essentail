575db7f4ae2e100bab71a39466bc20f3e23202c0
ezirmusitua <jferroal@gmail.com>
2018-06-25 19:26:41 +0800

    Fix: debugging docker volumes

--- data-container/generate.sh
+++ data-container/generate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo docker run --name $1 -v $2:$3 ubuntu:16.04 echo "Data-only container generated, use it with --volumes-from while running image"
+sudo docker volume create $1
