ca74b7981e96f0a13dc7405f29a7363b4ddfe29e
ezirmusitua <jferroal@gmail.com>
2018-07-09 22:16:01 +0800

    Feat: update nginx certbot

--- nginx-certbot/init.sh
+++ nginx-certbot/init.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# handling cert webroot nginx config and other conf
-# sed -i 's/#domain#/$1/g' cert.conf
-# mv cert.conf /etc/nginx/conf.d/
-# sed -i '$d' /etc/nginx/nginx.conf
-# sed -i '$a\ \ \ \ include '$4';' /etc/nginx/nginx.conf
-# sed -i '$a}' /etc/nginx/nginx.conf
+mv -v nginx-confs/* /etc/nginx/conf.d/
 
-# move cert-webroot to home
-# mv cert-webroot /root/
+mv cert-webroot /root/
 
 # reload nginx
-# nginx -s reload
+nginx -s reload
 
 # exec cerbot
 certbot -d ezirmusitua.com,www.ezirmusitua.com --authenticator webroot --installer nginx -n -m jferroal@gmail.com --agree-tos
