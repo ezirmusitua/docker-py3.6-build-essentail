a14ede569fca28f9badecca65f63d93f36e77172
ezirmusitua <jferroal@gmail.com>
2018-07-10 08:13:48 +0800

    Feat: update nginx certbot

--- nginx-certbot/init.sh
+++ nginx-certbot/init.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-mv -v nginx-confs/* /etc/nginx/conf.d/
+cp nginx-confs/* /etc/nginx/conf.d/
 
-mv cert-webroot /root/
+cp -r cert-webroot /root/
 
 # reload nginx
-nginx -s reload
+nginx
 
 # exec cerbot
-certbot -d ezirmusitua.com,www.ezirmusitua.com --authenticator webroot --installer nginx -n -m jferroal@gmail.com --agree-tos
+certbot -n -d ezirmusitua.com,www.ezirmusitua.com\
+    --authenticator webroot  webroot-map = {"ezirmusitua.site,www.ezirmusitua.site":"/root/cert-webroot"}\
+    --installer nginx -m jferroal@gmail.com --agree-tos
 
 # add renew cert cron task
 crontab -u root autorenew.txt
 
 # reload nginx
-nginx -s reload
+nginx
